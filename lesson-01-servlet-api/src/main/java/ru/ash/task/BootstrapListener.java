8c25ffb25586d63a159ee135934bec7d7c995b87
Belf <sleven182@mail.ru>
2021-06-18 09:46:19 +0300

    add servlet response

---
+++ lesson-01-servlet-api/src/main/java/ru/ash/task/BootstrapListener.java
@@ -0,0 +1,32 @@
+package ru.ash.task;
+
+import ru.ash.task.persist.Product;
+import ru.ash.task.persist.ProductRepository;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+@WebListener
+public class BootstrapListener implements ServletContextListener {
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        ServletContext sc = sce.getServletContext();
+
+        ProductRepository productRepository = new ProductRepository();
+        productRepository.save(new Product(1L,"Oranges", 160));
+        productRepository.save(new Product(2L,"Cherry", 100));
+        productRepository.save(new Product(3L,"Blueberry", 200));
+        productRepository.save(new Product(4L,"Watermelons", 150));
+        productRepository.save(new Product(5L,"Peaches", 120));
+        productRepository.save(new Product(6L,"Tomato", 300));
+        productRepository.save(new Product(7L,"cucumbers", 70));
+        productRepository.save(new Product(8L,"Cabbage", 80));
+        productRepository.save(new Product(9L,"Meat", 280));
+        productRepository.save(new Product(10L,"Water", 10));
+
+        sc.setAttribute("productRepository", productRepository);
+
+    }
+}
---
+++ lesson-01-servlet-api/src/main/java/ru/ash/task/persist/Product.java
@@ -0,0 +1,30 @@
+package ru.ash.task.persist;
+
+public class Product {
+    private Long id;
+    private String title;
+    private int cost;
+
+    public Product(Long id, String title, int cost) {
+        this.id = id;
+        this.title = title;
+        this.cost = cost;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+}
