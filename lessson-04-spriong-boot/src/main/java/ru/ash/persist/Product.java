788a0bea25d9ccba62e149020fc67ef686a574c4
Belf <sleven182@mail.ru>
2021-06-28 23:26:02 +0300

    add task4

---
+++ lessson-04-spriong-boot/src/main/java/ru/ash/persist/Product.java
@@ -0,0 +1,56 @@
+package ru.ash.persist;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+
+public class Product {
+
+    private Long id;
+    @NotBlank
+    private String title;
+    
+    private int cost;
+
+    public Product(){
+    }
+
+    public Product(Long id, String title, Integer cost) {
+        this.id = id;
+        this.title = title;
+        this.cost = cost;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", cost=" + cost +
+                '}';
+    }
+}
---
+++ lessson-04-spriong-boot/src/test/java/ru/ash/Lessson04SpriongBootApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class Lessson04SpriongBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
