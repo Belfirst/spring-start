dbeb3f98f9430821b16edda0abf663c2d86a4b28
Belf <sleven182@mail.ru>
2021-07-26 23:11:44 +0300

    add task11

--- lessson-04-spriong-boot/src/main/java/ru/ash/Lessson04SpriongBootApplication.java
+++ lessson-04-spriong-boot/src/main/java/ru/ash/Lessson04SpriongBootApplication.java
@@ -2,9 +2,19 @@ package ru.ash;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.net.PasswordAuthentication;
 
 @SpringBootApplication
 public class Lessson04SpriongBootApplication {
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Lessson04SpriongBootApplication.class, args);
